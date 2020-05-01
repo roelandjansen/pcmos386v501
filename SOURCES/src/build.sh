0244d8130237db3107867c41eacf742a56af8cca
Stas Sergeev <stsp@users.sourceforge.net>
2020-05-01 13:33:38 +0300

    build.sh: stop deleting *.SYS
    Since commit 24260aef they are permanently removed.
    Since commit a6b1f39d another dir ("latest") is used.

--- SOURCES/src/build.sh
+++ SOURCES/src/build.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -eu
+
 REV="$(dosemu --version | grep Revision | cut -d " " -f 2)"
 if [ -z "$REV" ] || [ "$REV" -lt 3684 ]; then
     echo "At least dosemu2 revision 3684 is required (see dosemu --version)."
     exit 1
 fi
 
-# need to remove uppercase *.SYS files as dosemu will create the
-# lowercase ones, they may collide
-rm -f kernel/*.SYS
-rm -f mos5src/*.SYS
 dosemu -td -K ./MAKEMOS.BAT -U 2 'path=%D\bin;%O'
